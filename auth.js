361f7a42adfc3f9a65509c006e3e4c8d57e0e9ef
Alex Vollmer <alex.vollmer@glutz..com>
2025-05-05 18:58:33 +0200

    spotify-follower-updates-backend: frontenduri2

--- auth.js
+++ auth.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI
+const FRONTEND_URI = process.env.FRONTEND_URI
 let accessToken = ''
 let refreshToken = ''
 let expiresAt = 0
