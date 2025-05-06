d3d9560f18c2d8c54bd754c18e212338d9e6165e
Alex Vollmer <alex.vollmer@glutz..com>
2025-05-06 11:00:17 +0200

    spotify-follower-updates-backend: socket

--- socket.js
+++ socket.js
@@ -1,8 +1,15 @@
-let io
+let io;
+
 module.exports = {
     init: server => {
-        io = require('socket.io')(server, {cors: {origin: '*'}});
-        return io
+        io = require('socket.io')(server, {
+            cors: {
+                origin: 'http://localhost:4200',
+                methods: ['GET', 'POST'],
+                credentials: true
+            }
+        });
+        return io;
     },
     get: () => io
-}
+};
