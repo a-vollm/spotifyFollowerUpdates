60008c318ea87f15dd90cd4b1a4252fe64c25cc4
Alex Vollmer <vollmeralex@gmx.ch>
2025-05-09 14:24:57 +0200

    spotify-follower-updates-backend: auth adjustment

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const {getCacheStatus, getReleases, getLatest, getPlaylistData} = require('./cache')
-const {ensureAccess} = require('./auth')
+const {sessions} = require('./auth')
 const router = express.Router()
 const subscriptions = [];
 
