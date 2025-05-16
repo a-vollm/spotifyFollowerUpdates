f9e27bc35a80ad41d29d4bb351b895b5f98d1790
Alex Vollmer <vollmeralex@gmx.ch>
2025-05-16 14:33:49 +0200

    spotify-follower-updates-backend: batch size for data

--- cache.js
+++ cache.js
@@ -20,9 +20,6 @@ function getCache(uid) {
 
 async function rebuild(uid, token) {
     const cache = getCache(uid);
-    while (cache.status.loading) {           // warten bis vorheriger Job fertig
-        await new Promise(r => setTimeout(r, 500));
-    }
     cache.status = {loading: true, totalArtists: 0, doneArtists: 0};
 
     const io = require('./socket').get?.();       // kann undefined sein
